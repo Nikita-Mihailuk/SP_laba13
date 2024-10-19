9f7874cb8d4136de98434dea2e234efd133252f6
Nikita-Mihailuk <mihnik.9898@gmail.com>
2024-10-19 09:21:22 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -49,6 +49,10 @@ public class Main {
             // Открываем файл
             Desktop.getDesktop().open(new File(fullPath));
 
+            // Освобождаем ресурсы
+            os.close();
+            is.close();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -58,23 +62,26 @@ public class Main {
     public static String getExtension(byte[] header) {
 
         Map<String, String> Extensions = new HashMap<>();
-        Extensions.put("FF D8 FF", ".jpg");  // JPEG
-        Extensions.put("89 50 4E", ".png");  // PNG
-        Extensions.put("49 44 33", ".mp3");  // MP3
-        Extensions.put("25 50 44", ".pdf");  // PDF
-        Extensions.put("00 00 00", ".mp4");  // MP4
-        Extensions.put("50 4B 03", ".docx"); // DOCX
+        Extensions.put("FF D8 FF", ".jpg");
+        Extensions.put("89 50 4E", ".png");
+        Extensions.put("49 44 33", ".mp3");
+        Extensions.put("25 50 44", ".pdf");
+        Extensions.put("00 00 00", ".mp4");
+        Extensions.put("50 4B 03", ".docx");
+        Extensions.put("47 49 46", ".gif");
+        Extensions.put("50 4B 07", ".zip");
+        Extensions.put("52 61 72", ".rar");
 
         // Преобразуем первые байты в строку шестнадцатиричных чисел
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 3; i++) { // Считаем первые 3 байта
-            sb.append(String.format("%02X ", header[i]));
+            sb.append(String.format("%02X ", header[i])); // %02X означает 16-тиричную систему
         }
 
-        // Преобразуем в строку для дальнейшей работы
+        // Преобразуем в строку для дальнейшей работы и удаляем пробел на конце, потому что символы прибавляются с пробелом
         String str = sb.toString().trim();
 
-        // Возвращаем расширение, если нашли соответствие
+        // Возвращаем расширение, если нашли соответствие, если нет, то возвращаем null
         return Extensions.getOrDefault(str, null);
     }
 }
